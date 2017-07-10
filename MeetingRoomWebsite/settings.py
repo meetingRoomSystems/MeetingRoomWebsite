7d63a1d35ffb680b379b28a486ce8cda3c955c99
compulynxmeetingroom <compulynxmeetingroom@gmail.com>
2017-07-10 09:48:52 +0300

    update database

--- MeetingRoomWebsite/settings.py
+++ MeetingRoomWebsite/settings.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print("base dir path", BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -79,6 +78,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
